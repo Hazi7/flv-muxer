3ef111acdd2484cfa1fa3db1bca77478b124cd8d
Hazi <83254065@qq.com>
2025-01-19 01:09:28 +0800

    refactor: 更新 AmfEncoder 的脚本数据格式方法

--- src/core/amf-encoder.ts
+++ src/core/amf-encoder.ts
@@ -1,38 +1,87 @@
 import { AmfType } from "../constants/amf-type";
 import { BinaryWriter } from "../utils/binary-writer";
 
-/**
- * AMF (Action Message Format) 编码器，用于将 JavaScript 对象编码为 AMF 格式的二进制数据。
- * 继承自 BinaryWriter，提供了一系列方法来编码不同类型的数据。
- */
 export class AmfEncoder extends BinaryWriter {
   /**
-   * 编码一个数字为 AMF 格式。
-   * @param num - 要编码的数字。
+   * 将 Date 对象以 AMF 格式写入二进制流。
+   * @param date - 要写入的 Date 对象。
    */
-  writeAmfNumber(num: number) {
-    this.writeUint8(AmfType.NUMBER);
-
-    this.writeFloat64(num);
+  writeScriptDataDate(date: Date) {
+    this.writeFloat64(date.getTime());
+    this.writeUint16(0); // 时区，默认为0
   }
 
   /**
-   * 编码一个布尔值为 AMF 格式。
-   * @param bool - 要编码的布尔值。
+   * 将 ECMA 数组以 AMF 格式写入二进制流。
+   * @param obj - 表示 ECMA 数组的对象。
    */
-  writeAmfBoolean(bool: boolean) {
-    this.writeUint8(AmfType.BOOLEAN);
+  writeScriptDataEcmaArray(obj: Record<string, any>) {
+    this.writeUint32(Object.keys(obj).length); // 数组长度
 
-    this.writeUint8(bool ? 0x01 : 0x00);
+    for (const [key, value] of Object.entries(obj)) {
+      this.writeScriptDataObjectProperty(key, value);
+    }
+
+    this.writeScriptDataObjectEnd();
   }
 
   /**
-   * 编码一个字符串为 AMF 格式。
-   * @param str - 要编码的字符串。
+   * 将长字符串以 AMF 格式写入二进制流。
+   * @param str - 要写入的字符串。
    */
-  writeAmfString(str: string) {
-    this.writeUint8(AmfType.STRING);
+  writeScriptDataLongString(str: string) {
+    const strBytes = new TextEncoder().encode(str);
+    this.writeUint32(strBytes.length);
+    this.writeBytes(strBytes);
+  }
 
+  /**
+   * 将对象以 AMF 格式写入二进制流。
+   * @param objs - 要写入的对象。
+   */
+  writeScriptDataObject(objs: Record<string, any>) {
+    for (const [key, value] of Object.entries(objs)) {
+      this.writeScriptDataObjectProperty(key, value);
+    }
+
+    this.writeScriptDataObjectEnd();
+  }
+
+  /**
+   * 将对象的结束标记以 AMF 格式写入二进制流。
+   */
+  writeScriptDataObjectEnd() {
+    this.writeUint8(0x00);
+    this.writeUint8(0x00);
+    this.writeUint8(0x09);
+  }
+
+  /**
+   * 将对象的属性以 AMF 格式写入二进制流。
+   * @param key - 属性的键。
+   * @param value - 属性的值。
+   */
+  writeScriptDataObjectProperty(key: string, value: any) {
+    this.writeScriptDataString(key);
+    this.writeScriptDataValue(value);
+  }
+
+  /**
+   * 将严格数组以 AMF 格式写入二进制流。
+   * @param arr - 要写入的数组。
+   */
+  writeScriptDataStrictArray(arr: any[]) {
+    this.writeUint32(arr.length);
+    for (const value of arr) {
+      this.writeScriptDataValue(value);
+    }
+  }
+
+  /**
+   * 将字符串以 AMF 格式写入二进制流。
+   * @param str - 要写入的字符串。
+   */
+  writeScriptDataString(str: string) {
     const encoder = new TextEncoder();
     const strBytes = encoder.encode(str);
 
@@ -41,141 +90,37 @@ export class AmfEncoder extends BinaryWriter {
   }
 
   /**
-   * 编码一个对象为 AMF 格式。
-   * @param objs - 要编码的对象。
+   * 将值以 AMF 格式写入二进制流。
+   * @param value - 要写入的值。
    */
-  writeAmfObject(objs: Record<string, any>) {
-    this.writeUint8(AmfType.OBJECT);
-
-    for (const [key, value] of Object.entries(objs)) {
-      this.writeAmfString(key);
-      this.writeAmfValue(value);
-    }
-
-    this.writeAmfObjectEndMarker();
-  }
-
-  /**
-   * 编码一个 MovieClip 为 AMF 格式。预留，不支持。
-   */
-  writeAmfMovieClip() {
-    this.writeUint8(AmfType.MOVIE_CLIP);
-  }
-
-  /**
-   * 编码一个 null 值为 AMF 格式。
-   */
-  writeAmfNull() {
-    this.writeUint8(AmfType.NULL); // NULL marker
-  }
-
-  /**
-   * 编码一个 undefined 值为 AMF 格式。
-   */
-  writeAmfUndefined(): void {
-    this.writeUint8(AmfType.UNDEFINED); // UNDEFINED marker
-  }
-
-  /**
-   * 编码一个引用为 AMF 格式。
-   * @param index - 引用的索引。
-   */
-  writeAmfReference(index: number) {
-    this.writeUint8(AmfType.REFERENCE);
-
-    this.writeUint16(index);
-  }
-
-  /**
-   * 编码一个 ECMA 数组为 AMF 格式。
-   * @param obj - 要编码的 ECMA 数组。
-   */
-  writeAmfECMAArray(obj: Record<string, any>): void {
-    this.writeUint8(AmfType.ECMA_ARRAY);
-
-    this.writeUint32(Object.keys(obj).length);
-
-    for (const [key, value] of Object.entries(obj)) {
-      this.writeAmfString(key);
-      this.writeAmfValue(value);
-    }
-
-    this.writeAmfObjectEndMarker();
-  }
-
-  /**
-   * 写入 AMF 对象结束标记。
-   */
-  writeAmfObjectEndMarker() {
-    this.writeUint8(AmfType.OBJECT_END_MARKER);
-
-    this.writeUint8(0x00);
-    this.writeUint8(0x00);
-    this.writeUint8(0x09);
-  }
-
-  /**
-   * 编码一个严格数组为 AMF 格式。
-   * @param arr - 要编码的数组。
-   */
-  writeAmfStrictArray(arr: any[]) {
-    this.writeUint8(AmfType.STRICT_ARRAY);
-
-    this.writeUint32(arr.length);
-    for (const value of arr) {
-      this.writeAmfValue(value);
-    }
-  }
-
-  /**
-   * 编码一个日期为 AMF 格式。
-   * @param date - 要编码的日期。
-   */
-  writeAmfDate(date: Date) {
-    this.writeUint8(AmfType.DATE); // DATE marker
-
-    this.writeFloat64(date.getTime());
-    this.writeInt16(0); // 时区，默认为 0
-  }
-
-  /**
-   * 编码一个长字符串为 AMF 格式。
-   * @param str - 要编码的长字符串。
-   */
-  writeAmfLongString(str: string) {
-    this.writeUint8(AmfType.LONG_STRING);
-
-    const strBytes = new TextEncoder().encode(str);
-
-    this.writeUint32(strBytes.length);
-    this.writeBytes(strBytes);
-  }
-
-  /**
-   * 编码一个值为 AMF 格式。
-   * @param value - 要编码的值。
-   */
-  writeAmfValue(value: any) {
+  writeScriptDataValue(value: any) {
     if (value === null) {
-      this.writeAmfNull();
+      this.writeUint8(AmfType.NULL);
     } else if (value === undefined) {
-      this.writeAmfUndefined();
+      this.writeUint8(AmfType.UNDEFINED);
     } else if (typeof value === "boolean") {
-      this.writeAmfBoolean(value);
+      this.writeUint8(AmfType.BOOLEAN);
+      this.writeUint8(value ? 0x01 : 0x00);
     } else if (typeof value === "number") {
-      this.writeAmfNumber(value);
+      this.writeUint8(AmfType.NUMBER);
+      this.writeFloat64(value);
     } else if (typeof value === "string") {
       if (value.length > 65535) {
-        this.writeAmfLongString(value);
+        this.writeUint8(AmfType.LONG_STRING);
+        this.writeScriptDataLongString(value);
       } else {
-        this.writeAmfString(value);
+        this.writeUint8(AmfType.STRING);
+        this.writeScriptDataString(value);
       }
     } else if (value instanceof Date) {
-      this.writeAmfDate(value);
+      this.writeUint8(AmfType.DATE);
+      this.writeScriptDataDate(value);
     } else if (Array.isArray(value)) {
-      this.writeAmfStrictArray(value);
+      this.writeUint8(AmfType.STRICT_ARRAY);
+      this.writeScriptDataStrictArray(value);
     } else if (typeof value === "object") {
-      this.writeAmfECMAArray(value);
+      this.writeUint8(AmfType.ECMA_ARRAY);
+      this.writeScriptDataEcmaArray(value);
     }
   }
 }
